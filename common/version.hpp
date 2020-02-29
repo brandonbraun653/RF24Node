c9e6cfaa104e03fdf2fd9a19bbe23280cdf65aef
Brandon Braun <brandonbraun653@gmail.com>
2020-02-28 23:26:30 -0600

    Add version file.
    Resolves #1

---
+++ common/version.hpp
@@ -0,0 +1,23 @@
+/********************************************************************************
+*  File Name:
+*    version.hpp
+*
+*  Description:
+*    RF24 software version information
+*
+*  2020 | Brandon Braun | brandonbraun653@gmail.com
+********************************************************************************/
+
+#pragma once
+#ifndef RF24_NODE_VERSION_HPP
+#define RF24_NODE_VERSION_HPP
+
+/* STL Includes */
+#include <string_view>
+
+namespace Chimera
+{
+  static constexpr std::string_view version = "1.0";
+}
+
+#endif	/* !RF24_NODE_VERSION_HPP */
